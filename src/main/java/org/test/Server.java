3598018dd0b979ae8f35333a220b2994fcf15f07
Jeffrey Blattman <jeffrey.blattman@gmail.com>
2011-09-22 16:49:11 +0200

    remove dead code

--- src/main/java/org/test/Server.java
+++ src/main/java/org/test/Server.java
@@ -114,20 +114,6 @@ public class Server implements Container {
 		}
 	}
 
-	TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-		public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-			return null;
-		}
-
-		public void checkClientTrusted(
-				java.security.cert.X509Certificate[] certs, String authType) {
-		}
-
-		public void checkServerTrusted(
-				java.security.cert.X509Certificate[] certs, String authType) {
-		}
-	} };
-
 	public synchronized void start() throws IOException {
 		connection = new SocketConnection(this);
 		SocketAddress address = new InetSocketAddress(port);
